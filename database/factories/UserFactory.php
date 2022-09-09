77fae1a7067c3565b6d7c8775620c5e284f25362
archanachandel <kumariarchana@seasia.in>
2022-09-09 11:02:41 +0530

    changes in seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,8 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserFactory extends Factory
 {
@@ -28,10 +30,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'designation'=> $this->faker->text(5),
             'company_id'=> rand(1,50),
-            'is_root_user'=>rand(0,1),
-            'parent_id'=>rand(1,50),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'is_root_user'=>'0',
+            'parent_id'=>'0',
+            'password' => Hash::make('mind@123'), // password
             'created_at'=> date('Y-m-d H:i:s',rand(1662100000,1662113343))
         ];
     }
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UserTableSeeder extends Seeder
@@ -17,17 +16,7 @@ class UserTableSeeder extends Seeder
     public function run()
 
     {
-        DB::table('users')->insert([
-            'id' => 1,
-            'name' => 'demo',
-            'email' => 'demo@yopmail.com',
-            'password' => Hash::make('mind@123'),
-            'designation' => 'developer',
-            'company_id' => '1',
-            'is_root_user' => '1',
-            'parent_id' => '1',
-            'created_at' => date('Y-m-d H:i:s', rand(1662100000, 1662113343))
-        ]);
+        
         User::factory()->count(50)->create();
     }
 }
